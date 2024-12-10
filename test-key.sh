5a7b95cb0ee3fea10e10969a2641ac2e833a506d
Dan Wing <danwing@gmail.com>
2024-12-10 08:55:31 -0800

    make test-key.sh work better on Linux

--- test-key.sh
+++ test-key.sh
@@ -3,15 +3,21 @@
 set -e
 name="test-key"
 
-if ! command -v gbasenc 2>&1 >/dev/null; then
-    echo "need to install gbasenc.  On MacOS do 'brew install coreutils'"
+encoder=""
+if command -v gbasenc 2>&1 >/dev/null; then encoder="gbasenc"; fi
+if command -v basenc 2>&1 >/dev/null; then encoder="basenc"; fi
+if [ -z "${encoder}" ]; then
+    echo "need to install basenc.  On MacOS do 'brew install coreutils', Linux 'apt install coreutils'"
     exit 1
 fi
 
 
 # if no public/private key file, generate it
 if [ ! -f $name.pem ]; then
+    echo info: generating fresh public/private key pair
     openssl genrsa -out $name.pem 2048
+else
+    echo info: re-using existing public/private key pair
 fi
 
 # generate public key file in PEM (for use in documentation)
@@ -21,8 +27,8 @@ openssl rsa -in $name.pem -pubout -outform der > $name.der 2>/dev/null
 
 
 sha256hash=$(shasum -b -a 256 < $name.der | awk '{print $1}')
-sha256url=$(echo -n $sha256hash | xxd -r -p | gbasenc --base64url | tr -d "=")
-sha256b32=$(echo -n $sha256hash | xxd -r -p | gbasenc --base32 | tr -d "=")
+sha256url=$(echo -n $sha256hash | xxd -r -p | $encoder --base64url | tr -d "=")
+sha256b32=$(echo -n $sha256hash | xxd -r -p | $encoder --base32 | tr -d "=")
 
 echo "sha256 hash is          $sha256hash"
 echo "sha256 base64url is     $sha256url"
@@ -30,15 +36,11 @@ echo "sha256 base32 is        $sha256b32"
 echo 
 
 sha512256hash=$(shasum -b -a 512256 < $name.der | awk '{print $1}')
-sha512246url=$(echo -n $sha512256hash | xxd -r -p | gbasenc --base64url | tr -d "=")
-sha512256b32=$(echo -n $sha512256hash | xxd -r -p | gbasenc --base32 | tr -d "=")
+sha512246url=$(echo -n $sha512256hash | xxd -r -p | $encoder --base64url | tr -d "=")
+sha512256b32=$(echo -n $sha512256hash | xxd -r -p | $encoder --base32 | tr -d "=")
 
 echo "sha512/256 hash is      $sha512256hash"
 echo "sha512/256 base64url is $sha512246url"
 echo "sha512/256 base32 is    $sha512256b32"
 
 exit
-
-
-
- 
